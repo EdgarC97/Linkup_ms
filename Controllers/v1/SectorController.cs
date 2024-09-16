31c7cf7d167ddb6e447186db544c205ac82564ff
Brayan Acosta <landakkpp@gmail.com>
2024-09-16 07:48:01 -0500

    add updating in folder data -add dbset sector and userrole - add endpoind getsectors

--- Controllers/v1/SectorController.cs
+++ Controllers/v1/SectorController.cs
@@ -2,11 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend_Riwi_LinkUp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Linkup_ms.Controllers.v1
 {
-    public class SectorController
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SectorController : ControllerBase
     {
-        
+        private readonly AppDbContext _context;
+
+        public SectorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get: api/Sector
+        [HttpGet]
+        public async Task<IActionResult> AllSectors()
+        {
+            try
+            {
+                var sectors = await _context.Sectors.ToListAsync();
+                return Ok(sectors);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while retrieving sectores.", error = ex.Message});
+            }
+        }
     }
 }
